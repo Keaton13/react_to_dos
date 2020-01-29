e812f59d4ef372ce4a15334ba834e377ffcd51c1
Keaton Krieger <bajabuggy313@gmail.com>
2020-01-28 19:53:12 -0800

    Completed rendering of ToDo Items on screen

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-ReactDOM.render(<h1>Hello React! CHANGE</h1>, document.getElementById('root'));
-4;
+ReactDOM.render(
+    <App/>,
+     document.getElementById('root')
+);
