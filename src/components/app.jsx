abe5bba988aa2f1666c4ecfaecfbc0a6827ce172
Keaton Krieger <bajabuggy313@gmail.com>
2020-02-10 20:15:44 -0800

    Working to do list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,73 @@
 import React from 'react';
 import ToDoList from './to_do_list';
-import Button from './button';
-import AddToDo from './add_to_do';
+import AddToDoForm from './add_to_do_form';
 
-function App(props) {
-  return (
-    <div>
-      <h1>To Do List</h1>
-      <ToDoList />
-      <AddToDo />
-    </div>
-  );
+const toDos = [
+  {
+    id: '01',
+    title: 'Wash car'
+  },
+  {
+    id: '02',
+    title: 'Go to store'
+  },
+  {
+    id: '03',
+    title: 'Clean bathroom'
+  },
+  {
+    id: '04',
+    title: 'Learn React'
+  }
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: []
+    };
+
+    this.addToDo = this.addToDo.bind(this)
+  }
+
+  componentDidMount(){
+    this.getToDos();
+  }
+
+  addToDo(item){
+    const { list } = this.state;
+
+    const _id = list.length + 1;
+
+    this.setState({
+      list: [
+        ...list,
+        {
+          _id: _id,
+          title: item.title
+        }
+      ]
+    });
+  }
+
+  getToDos() {
+    this.setState({
+      list: toDos
+    })
+  }
+  render() {
+    const { list } = this.state;
+
+    return (
+      <div>
+        <h1>To Do List</h1>
+        <ToDoList list={list} />
+        <AddToDoForm add={this.addToDo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
